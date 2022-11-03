78f28fa4d1ad00d1b5158e23808ad2d4d3275e25
Verkhovskiy <artemverhovskiy@itcenterifmo.onmicrosoft.com>
2022-11-03 22:39:02 +0200

    Task 7 and 8 completed

---
+++ ITMO.cscourse2022.lab08/ITMO.cscourse2022.lab08.task02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.cscourse2022.lab08.task02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
